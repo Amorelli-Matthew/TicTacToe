24ea88c7eba3ab212dec1b6c4001d9f2dbf296fd
Amorelli-Matthew <mattam.business@yahoo.com>
2025-09-23 19:25:03 -0700

    uploaded TicTacToe project files

---
+++ TicTacToe/Form1.Designer.cs
@@ -0,0 +1,226 @@
+﻿
+namespace TicTacToe
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.buttonNewGame = new System.Windows.Forms.Button();
+            this.XorO = new System.Windows.Forms.ImageList(this.components);
+            this.labelGame1 = new System.Windows.Forms.Label();
+            this.labelGame2 = new System.Windows.Forms.Label();
+            this.labelGame4 = new System.Windows.Forms.Label();
+            this.labelGame5 = new System.Windows.Forms.Label();
+            this.labelGame6 = new System.Windows.Forms.Label();
+            this.labelGame7 = new System.Windows.Forms.Label();
+            this.labelGame8 = new System.Windows.Forms.Label();
+            this.labelGame9 = new System.Windows.Forms.Label();
+            this.labelGame3 = new System.Windows.Forms.Label();
+            this.labelTextWinner = new System.Windows.Forms.Label();
+            this.labelTittle = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // buttonNewGame
+            // 
+            this.buttonNewGame.BackColor = System.Drawing.Color.LightCyan;
+            this.buttonNewGame.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonNewGame.Location = new System.Drawing.Point(13, 383);
+            this.buttonNewGame.Name = "buttonNewGame";
+            this.buttonNewGame.Size = new System.Drawing.Size(155, 66);
+            this.buttonNewGame.TabIndex = 18;
+            this.buttonNewGame.Text = "New Game";
+            this.buttonNewGame.UseVisualStyleBackColor = false;
+            this.buttonNewGame.Click += new System.EventHandler(this.buttonNewGame_Click);
+            // 
+            // XorO
+            // 
+            this.XorO.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("XorO.ImageStream")));
+            this.XorO.TransparentColor = System.Drawing.Color.Transparent;
+            this.XorO.Images.SetKeyName(0, "699px-Deseret_capital_long_O.svg.png");
+            this.XorO.Images.SetKeyName(1, "1200px-LetterX.svg.png");
+            // 
+            // labelGame1
+            // 
+            this.labelGame1.Font = new System.Drawing.Font("Microsoft Sans Serif", 50.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelGame1.Location = new System.Drawing.Point(31, 70);
+            this.labelGame1.Name = "labelGame1";
+            this.labelGame1.Size = new System.Drawing.Size(71, 75);
+            this.labelGame1.TabIndex = 45;
+            this.labelGame1.Text = "X";
+            // 
+            // labelGame2
+            // 
+            this.labelGame2.Font = new System.Drawing.Font("Microsoft Sans Serif", 50.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelGame2.Location = new System.Drawing.Point(164, 70);
+            this.labelGame2.Name = "labelGame2";
+            this.labelGame2.Size = new System.Drawing.Size(71, 75);
+            this.labelGame2.TabIndex = 46;
+            this.labelGame2.Text = "X";
+            // 
+            // labelGame4
+            // 
+            this.labelGame4.Font = new System.Drawing.Font("Microsoft Sans Serif", 50.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelGame4.Location = new System.Drawing.Point(31, 170);
+            this.labelGame4.Name = "labelGame4";
+            this.labelGame4.Size = new System.Drawing.Size(71, 75);
+            this.labelGame4.TabIndex = 48;
+            this.labelGame4.Text = "X";
+            // 
+            // labelGame5
+            // 
+            this.labelGame5.Font = new System.Drawing.Font("Microsoft Sans Serif", 50.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelGame5.Location = new System.Drawing.Point(164, 170);
+            this.labelGame5.Name = "labelGame5";
+            this.labelGame5.Size = new System.Drawing.Size(71, 75);
+            this.labelGame5.TabIndex = 49;
+            this.labelGame5.Text = "X";
+            // 
+            // labelGame6
+            // 
+            this.labelGame6.Font = new System.Drawing.Font("Microsoft Sans Serif", 50.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelGame6.Location = new System.Drawing.Point(287, 170);
+            this.labelGame6.Name = "labelGame6";
+            this.labelGame6.Size = new System.Drawing.Size(71, 75);
+            this.labelGame6.TabIndex = 50;
+            this.labelGame6.Text = "X";
+            // 
+            // labelGame7
+            // 
+            this.labelGame7.Font = new System.Drawing.Font("Microsoft Sans Serif", 50.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelGame7.Location = new System.Drawing.Point(31, 266);
+            this.labelGame7.Name = "labelGame7";
+            this.labelGame7.Size = new System.Drawing.Size(71, 75);
+            this.labelGame7.TabIndex = 51;
+            this.labelGame7.Text = "X";
+            // 
+            // labelGame8
+            // 
+            this.labelGame8.Font = new System.Drawing.Font("Microsoft Sans Serif", 50.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelGame8.Location = new System.Drawing.Point(164, 266);
+            this.labelGame8.Name = "labelGame8";
+            this.labelGame8.Size = new System.Drawing.Size(71, 75);
+            this.labelGame8.TabIndex = 52;
+            this.labelGame8.Text = "X";
+            // 
+            // labelGame9
+            // 
+            this.labelGame9.Font = new System.Drawing.Font("Microsoft Sans Serif", 50.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelGame9.Location = new System.Drawing.Point(287, 266);
+            this.labelGame9.Name = "labelGame9";
+            this.labelGame9.Size = new System.Drawing.Size(71, 75);
+            this.labelGame9.TabIndex = 53;
+            this.labelGame9.Text = "X";
+            // 
+            // labelGame3
+            // 
+            this.labelGame3.Font = new System.Drawing.Font("Microsoft Sans Serif", 50.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelGame3.Location = new System.Drawing.Point(287, 70);
+            this.labelGame3.Name = "labelGame3";
+            this.labelGame3.Size = new System.Drawing.Size(71, 75);
+            this.labelGame3.TabIndex = 47;
+            this.labelGame3.Text = "X";
+            // 
+            // labelTextWinner
+            // 
+            this.labelTextWinner.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.labelTextWinner.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelTextWinner.Location = new System.Drawing.Point(33, 341);
+            this.labelTextWinner.Name = "labelTextWinner";
+            this.labelTextWinner.Size = new System.Drawing.Size(325, 40);
+            this.labelTextWinner.TabIndex = 54;
+            this.labelTextWinner.Text = "The Winner!";
+            this.labelTextWinner.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // labelTittle
+            // 
+            this.labelTittle.AutoSize = true;
+            this.labelTittle.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelTittle.Location = new System.Drawing.Point(41, 32);
+            this.labelTittle.Name = "labelTittle";
+            this.labelTittle.Size = new System.Drawing.Size(302, 33);
+            this.labelTittle.TabIndex = 55;
+            this.labelTittle.Text = "Tic Tac Toe Simulater";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.LightCyan;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(203, 383);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(155, 66);
+            this.button1.TabIndex = 56;
+            this.button1.Text = "Exit";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.PeachPuff;
+            this.ClientSize = new System.Drawing.Size(401, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.labelTittle);
+            this.Controls.Add(this.labelTextWinner);
+            this.Controls.Add(this.labelGame9);
+            this.Controls.Add(this.labelGame8);
+            this.Controls.Add(this.labelGame7);
+            this.Controls.Add(this.labelGame6);
+            this.Controls.Add(this.labelGame5);
+            this.Controls.Add(this.labelGame4);
+            this.Controls.Add(this.labelGame3);
+            this.Controls.Add(this.labelGame2);
+            this.Controls.Add(this.labelGame1);
+            this.Controls.Add(this.buttonNewGame);
+            this.Name = "Form1";
+            this.Text = "Tic Tac Toe Simulator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button buttonNewGame;
+        private System.Windows.Forms.ImageList XorO;
+        private System.Windows.Forms.Label labelGame1;
+        private System.Windows.Forms.Label labelGame2;
+        private System.Windows.Forms.Label labelGame4;
+        private System.Windows.Forms.Label labelGame5;
+        private System.Windows.Forms.Label labelGame6;
+        private System.Windows.Forms.Label labelGame7;
+        private System.Windows.Forms.Label labelGame8;
+        private System.Windows.Forms.Label labelGame9;
+        private System.Windows.Forms.Label labelGame3;
+        private System.Windows.Forms.Label labelTextWinner;
+        private System.Windows.Forms.Label labelTittle;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ TicTacToe/Form1.cs
@@ -0,0 +1,396 @@
+﻿using System;
+using System.Windows.Forms;
+namespace TicTacToe
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        //Name: matthew Amorelli
+        //Date: November 9, 2021
+        //Description: This program is a Game where the user gets to watch a game of tictactoe happen
+        //and see if the X player wins or the O player Wins First because the Other player will fill up the board 
+        //out of frustration
+
+        private void buttonNewGame_Click(object sender, EventArgs e)
+        {
+            //try statement
+            try
+            {
+                //varables for the size of array
+                int GameArrayRows = 3;
+                int GameArrayCols = 3;
+
+                //decalring the array
+                int[,] GameArray = { { 1, 2, 3},
+                { 4, 5, 6},
+                { 7, 8, 9} };
+
+
+                //new random for the random number generator between 1 and 0
+                Random randomGame = new Random();
+
+                //assign a random number for each element
+
+                //start the began the game method
+                BegainGame(GameArrayRows, GameArrayCols, ref randomGame, GameArray);
+
+
+                ////test each element if its a X or a O aka 1 or 0
+                TestEachElementInArray(GameArray);
+
+
+                //Display the results
+                DisplayTheWinner(GameArray);
+
+            }
+            //catch statement
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+
+        /// <summary>
+        /// this method begans the game by assigning random numbers
+        /// </summary>
+        /// <param name="GameArrayRows"></param>
+        /// <param name="GameArrayCols"></param>
+        /// <param name="randomGame"></param>
+        /// <param name="GameArray"></param>
+        private void BegainGame(int GameArrayRows, int GameArrayCols, ref Random randomGame, int[,] GameArray)
+        {
+            for (int row = 0; row < GameArrayRows; ++row)
+            {
+                for (int col = 0; col < GameArrayCols; ++col)
+                {
+                    int xorO = randomGame.Next(0, 2);
+                    GameArray[row, col] = xorO;
+
+                }
+            }
+        }
+
+
+        /// <summary>
+        /// Test each element in the GameArray
+        /// </summary>
+        /// <param name="GameArray"></param>
+        /// <param name=""></param>
+        private void TestEachElementInArray(int[,] GameArray)
+        {
+
+            //for each of the elements in the game array, there is a switch statement that is being tested
+            //and then if its either  1 or 0 then an X or O gets showen up in the array
+            //as well as the  varable gets assign a X or a O
+            switch (GameArray[0, 0])
+            {
+                case 0:
+                    labelGame1.Text = "O";
+
+                    break;
+
+
+                case 1:
+                    labelGame1.Text = "X";
+
+                    break;
+            }
+
+            switch (GameArray[0, 1])
+            {
+                case 0:
+                    labelGame2.Text = "O";
+
+                    break;
+
+
+                case 1:
+                    labelGame2.Text = "X";
+
+                    break;
+            }
+
+            switch (GameArray[0, 2])
+            {
+                case 0:
+                    labelGame3.Text = "O";
+
+                    break;
+
+
+                case 1:
+                    labelGame2.Text = "X";
+
+                    break;
+            }
+
+
+            switch (GameArray[1, 0])
+            {
+                case 0:
+                    labelGame4.Text = "O";
+
+                    break;
+
+
+                case 1:
+                    labelGame4.Text = "X";
+
+                    break;
+            }
+
+            switch (GameArray[1, 1])
+            {
+                case 0:
+                    labelGame5.Text = "O";
+
+                    break;
+
+
+                case 1:
+                    labelGame5.Text = "X";
+
+                    break;
+            }
+
+            switch (GameArray[1, 2])
+            {
+                case 0:
+                    labelGame6.Text = "O";
+
+                    break;
+
+
+                case 1:
+                    labelGame6.Text = "X";
+
+                    break;
+            }
+
+            switch (GameArray[2, 0])
+            {
+                case 0:
+                    labelGame7.Text = "O";
+
+                    break;
+
+
+                case 1:
+                    labelGame7.Text = "X";
+
+                    break;
+            }
+
+            switch (GameArray[2, 1])
+            {
+                case 0:
+                    labelGame8.Text = "O";
+
+                    break;
+
+
+                case 1:
+                    labelGame8.Text = "X";
+
+                    break;
+            }
+
+            switch (GameArray[2, 2])
+            {
+                case 0:
+                    labelGame9.Text = "O";
+
+                    break;
+
+
+                case 1:
+                    labelGame9.Text = "X";
+
+                    break;
+            }
+
+
+        }
+
+        /// <summary>
+        /// this sums up if a row matches
+        /// </summary>
+        /// <param name="GameArray"></param>
+        /// <param name=""></param>
+        private void DisplayTheWinner(int[,] GameArray)
+        {
+            //with each if, it checks if a certain row has all equal matches then shows the winner
+            //each comment before it's if statement indicates witch pattern wins
+
+            //top row
+            // * * *
+            if (GameArray[0, 0] == 1 && GameArray[0, 1] == 1 && GameArray[0, 2] == 1)
+            {
+                labelTextWinner.Text = "X Wins First!!!";
+            }
+            else if (GameArray[0, 0] == 0 && GameArray[0, 1] == 0 && GameArray[0, 2] == 0)
+            {
+                labelTextWinner.Text = "O Wins First!!!";
+            }
+
+
+
+
+
+            //middle row
+            // * * * 
+            else if (GameArray[1, 0] == 1 && GameArray[1, 1] == 1 && GameArray[1, 2] == 1)
+            {
+                labelTextWinner.Text = "X Wins First!!!";
+            }
+            else if (GameArray[1, 0] == 0 && GameArray[1, 1] == 0 && GameArray[1, 2] == 0)
+            {
+                labelTextWinner.Text = "O Wins First!!!";
+            }
+
+
+            //bottom row
+            //* * *
+            else if (GameArray[2, 0] == 1 && GameArray[2, 1] == 1 && GameArray[2, 2] == 1)
+            {
+                labelTextWinner.Text = "X Wins First!!!";
+            }
+            else if (GameArray[2, 0] == 0 && GameArray[2, 1] == 0 && GameArray[2, 2] == 0)
+            {
+                labelTextWinner.Text = "O Wins First!!!";
+            }
+
+
+
+            //diaginal from left top to right bottem
+            /*
+                *
+                    */
+            else if (GameArray[1, 0] == 1 && GameArray[1, 1] == 1 && GameArray[2, 2] == 1)
+            {
+                labelTextWinner.Text = "X Wins First!!!";
+            }
+            else if (GameArray[1, 0] == 0 && GameArray[1, 1] == 0 && GameArray[2, 2] == 0)
+            {
+                labelTextWinner.Text = "O Wins First!!!";
+            }
+
+
+            //diaginal from right top to left bottom
+            //      *
+            //   *
+            // *
+            else if (GameArray[0, 2] == 1 && GameArray[1, 1] == 1 && GameArray[2, 0] == 1)
+            {
+                labelTextWinner.Text = "X Wins First!!!";
+            }
+            else if (GameArray[0, 2] == 0 && GameArray[1, 1] == 0 && GameArray[2, 0] == 0)
+            {
+                labelTextWinner.Text = "O Wins First!!!";
+            }
+
+
+            /* o col
+             *
+             */
+            else if (GameArray[0, 0] == 1 && GameArray[1, 0] == 1 && GameArray[2, 0] == 1)
+            {
+                labelTextWinner.Text = "X Wins First!!!";
+            }
+            else if (GameArray[0, 0] == 1 && GameArray[1, 0] == 1 && GameArray[2, 0] == 1)
+            {
+                labelTextWinner.Text = "O Wins First!!!";
+            }
+
+
+            /* 1 col
+             *
+             */
+
+            else if (GameArray[0, 1] == 1 && GameArray[1, 1] == 1 && GameArray[2, 1] == 1)
+            {
+                labelTextWinner.Text = "X Wins First!!!";
+            }
+
+            else if (GameArray[0, 1] == 0 && GameArray[1, 1] == 0 && GameArray[2, 1] == 0)
+            {
+                labelTextWinner.Text = "O Wins First!!!";
+            }
+
+
+            /* 2 col
+             *
+             */
+            else if (GameArray[0, 2] == 1 && GameArray[1, 2] == 1 && GameArray[2, 2] == 1)
+            {
+                labelTextWinner.Text = "X Wins First!!!";
+            }
+            else if (GameArray[0, 2] == 0 && GameArray[1, 2] == 0 && GameArray[2, 2] == 0)
+            {
+                labelTextWinner.Text = "O Wins First!!!";
+            }
+
+
+            //Special ties
+            //if both top and middle rows get tie
+            else if (GameArray[0, 0] == 1 && GameArray[0, 1] == 1 && GameArray[0, 2] == 1 && GameArray[1, 0] == 1 && GameArray[1, 1] == 1 && GameArray[1, 2] == 1)
+            {
+                labelTextWinner.Text = "Tie";
+            }
+            else if (GameArray[0, 0] == 0 && GameArray[0, 1] == 0 && GameArray[0, 2] == 0 && GameArray[1, 0] == 0 && GameArray[1, 1] == 0 && GameArray[1, 2] == 0)
+            {
+                labelTextWinner.Text = "Tie";
+            }
+
+
+            //if Both middle and bottom get tie
+            else if (GameArray[1, 0] == 1 && GameArray[1, 1] == 1 && GameArray[1, 2] == 1 && GameArray[2, 0] == 1 && GameArray[2, 1] == 1 && GameArray[2, 2] == 1)
+            {
+                labelTextWinner.Text = "Tie";
+            }
+            else if (GameArray[1, 0] == 0 && GameArray[1, 1] == 0 && GameArray[1, 2] == 0 && GameArray[1, 1] == 0 && GameArray[1, 2] == 0)
+            {
+                labelTextWinner.Text = "Tie";
+            }
+
+
+            //if both zero col and first col get a tie
+            else if (GameArray[0, 0] == 1 && GameArray[1, 0] == 1 && GameArray[2, 0] == 1 && GameArray[0, 1] == 1 && GameArray[1, 1] == 1 && GameArray[2, 1] == 1)
+            {
+                labelTextWinner.Text = "Tie";
+            }
+            else if (GameArray[0, 0] == 0 && GameArray[0, 1] == 0 && GameArray[0, 2] == 0 && GameArray[0, 1] == 0 && GameArray[1, 1] == 0 && GameArray[2, 1] == 0)
+            {
+                labelTextWinner.Text = "Tie";
+            }
+
+
+            //if Both first col and second col get tie
+            else if (GameArray[0, 1] == 1 && GameArray[1, 1] == 1 && GameArray[2, 1] == 1 && GameArray[0, 2] == 1 && GameArray[1, 2] == 1 && GameArray[2, 2] == 1)
+            {
+                labelTextWinner.Text = "Tie";
+            }
+            else if (GameArray[0, 1] == 0 && GameArray[1, 1] == 0 && GameArray[2, 1] == 0 && GameArray[0, 2] == 0 && GameArray[1, 2] == 0 && GameArray[2, 2] == 0)
+            {
+                labelTextWinner.Text = "Tie";
+            }
+
+            else
+            {
+                labelTextWinner.Text = "It's a Tie";
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //exit the game
+            Application.Exit();
+        }
+    }
+}
